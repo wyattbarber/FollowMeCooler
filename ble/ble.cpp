c00dd5e6ac5535b26d603b81eb51d4013da3fab2
Wyatt Barber <wyattjbarber@gmail.com>
2023-11-19 09:36:05 -0500

    Coordinate reception from  MIT App Inventor working

--- ble/ble.cpp
+++ ble/ble.cpp
@@ -1,39 +1,98 @@
 #include <cstdlib>
 #include <cstdio>
 #include <vector>
+#include <string>
 #define _USE_MATH_DEFINES
 #include <cmath>
 #include "pico/stdlib.h"
 #include "hardware/uart.h"
 
-#define BLE_UART uart0
-#define BLE_IRQ UART0_IRQ
-#define BLE_TX 0
-#define BLE_RX 1
+#define BLE_UART uart1
+#define BLE_IRQ UART1_IRQ
+#define BLE_TX 4
+#define BLE_RX 5
 #define BLE_DATABITS 8
 #define BLE_STOPBITS 1
 #define BLE_PARITY UART_PARITY_NONE
+#define BLE_PIN_CTS 2
+#define BLE_PIN_RTS 3
 
+std::string ui_msg = {""};
+#define UI_CODE_POS 'C'
+#define UI_CODE_MODE 'M'
+#define UI_MODE_HOLD "H"
+#define UI_MODE_FOLLOW "F"
+#define UI_MODE_TEST_DRIVE "TD"
+#define UI_MODE_TEST_OA "TOA"
 
-
-void ble_rx_handle()
+long latitude;
+long longitude;
+typedef enum
 {
-    while(uart_is_readable(BLE_UART))
+    HOLD,
+    FOLLOW,
+    DRIVE_TEST,
+    OA_TEST
+} OpMode;
+OpMode mode = HOLD;
+
+void decode_msg(char cmd, std::string data)
+{
+    switch (cmd)
     {
-        if(uart_getc(BLE_UART) == 'T')
-        {
-            gpio_xor_mask(1 << 25);
-        }
+    case UI_CODE_POS:
+    {
+        size_t split = data.find(':');
+        latitude = std::stol(data);
+        longitude = std::stol(data.substr(split + 1));
+        break;
+    }
+
+    case UI_CODE_MODE:
+        if (data.compare(UI_MODE_HOLD))
+            mode = HOLD;
+        else if (data.compare(UI_MODE_FOLLOW))
+            mode = FOLLOW;
+        else if (data.compare(UI_MODE_TEST_DRIVE))
+            mode = DRIVE_TEST;
+        else if (data.compare(UI_MODE_TEST_OA))
+            mode = OA_TEST;
+        else
+            printf("Unrecognized mode %s", data.c_str());
+        break;
+
+    default:
+        printf("Unrecognized command %c", cmd);
+        break;
     }
 }
 
+void ble_rx_handle()
+{
+    while (uart_is_readable(BLE_UART))
+    {
+        char c = uart_getc(BLE_UART);
+        if (c == '\n')
+        {
+            // Decode input message
+            decode_msg(ui_msg[0], ui_msg.substr(1));
+            ui_msg = {""};
+        }
+        else
+        {
+            ui_msg.append(1, c);
+        }
+        printf("%c", c);
+    }
+}
 
 int main()
 {
     stdio_init_all();
 
-    gpio_init(25);
-    gpio_set_dir(25, GPIO_OUT);
+    gpio_init_mask((1 << BLE_PIN_CTS) | (1 << BLE_PIN_RTS));
+    gpio_set_dir(BLE_PIN_CTS, GPIO_OUT);
+    gpio_set_dir(BLE_PIN_RTS, GPIO_IN);
 
     uart_init(BLE_UART, 9600);
     gpio_set_function(BLE_TX, GPIO_FUNC_UART);
@@ -43,9 +102,11 @@ int main()
     irq_set_enabled(BLE_IRQ, true);
     uart_set_irq_enables(BLE_UART, true, false);
 
-    while(true)
+    gpio_clr_mask(1 << BLE_PIN_CTS);
+
+    while (true)
     {
-        sleep_ms(1000);
-        // uart_puts(BLE_UART, "Hello?\n");
+        sleep_ms(5000);
+        printf("%d lat x %d long, mode #%d\n", latitude, longitude, mode);
     }
 }
